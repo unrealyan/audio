dc46ef826a9458d7c316f9e18f58dbace63cbaa4
heyan <unrealyan@gmail.com>
2017-09-21 14:48:37 +0800

    add npm

---
+++ app.js
@@ -0,0 +1,13 @@
+
+'use strict';
+
+document.write("It's working.");
+
+// This results in a warning:
+if (!window) {
+  // eslint-disable-next-line
+  require(`./${window}parseable.js`);
+}
+
+// This results in an error:
+// if(!window) require("test");
---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+
+'use strict';
+
+module.exports = {
+  context: __dirname,
+  entry: './app.js',
+  output: {
+    filename: 'bundle.js'
+  }
+};
